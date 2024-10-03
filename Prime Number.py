0e0d583a4cb948200e3d58e2ee695a49a6120da9
DivyeshYadav <prabhakaryadav621@gmail.com>
2024-10-03 15:00:55 +0545

    Prime Number

---
+++ Prime Number.py
@@ -0,0 +1,10 @@
+def prime_genretor():
+    for i in range(1,10001):
+        count = 0
+        for j in range(1,i+1):
+            if(i % j ==0):
+                count = count+1
+                if(count == 2):
+                    print(i,end='  ')
+prime_genretor()
+
